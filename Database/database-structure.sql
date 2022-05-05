9db490a7bfdd57252c9cabf875e0d345e8f5767f
Germán <germanariasrodriguez@gmail.com>
2022-05-05 14:51:30 +0200

    Update database-structure.sql
    Creation of de database structure

--- Database/database-structure.sql
+++ Database/database-structure.sql
@@ -1,3 +1,33 @@
 DROP TABLE IF EXISTS `users`;
 
-CREATE TABLE `users` ();
+CREATE TABLE `users` (
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Created at Time',
+    `user_name` VARCHAR(150) COMMENT 'User Name',
+    `type` CHAR(1) COMMENT 'Type of user, could be B(buyer), S(seller) or H(hybrid)',
+    PRIMARY KEY(`id`)
+);
+
+DROP TABLE IF EXISTS `settings`;
+
+CREATE TABLE `settings` (
+    `set_id` INT NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+    `user_id` INT COMMENT 'Foreign Key',
+    `user_img` TEXT COMMENT 'URL of the image',
+    `user_phone` VARCHAR(31) COMMENT 'User Phone Number',
+    `user_pass`  VARCHAR(15) NOT NULL COMMENT 'User Password',
+    PRIMARY KEY(`set_id`),
+    FOREIGN KEY (`user_id`) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+    `post_id` INT NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+    `user_id` INT COMMENT 'Foreign Key',
+    `title` VARCHAR(100) NOT NULL COMMENT 'Title',
+    `content` TEXT NOT NULL COMMENT 'Content of the post',
+    `post_date` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Post creation date',
+    PRIMARY KEY(`post_id`),
+    FOREIGN KEY (`user_id`) REFERENCES users(id)
+);
