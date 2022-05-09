b3f551b25bdd92a9890da1aea35eb0b33353b492
Germán <germanariasrodriguez@gmail.com>
2022-05-09 23:39:27 +0200

    Update database-structure.sql
    userExists success, also added a boolean to show it or not.
    
    Check the comment

--- Database/database-structure.sql
+++ Database/database-structure.sql
@@ -41,21 +41,77 @@ CREATE TABLE `posts` (
 -- Create database, tables and structure
 
 -- STORED PROCEDURES
+
 DELIMITER //
 
 -- userExists PROCEDURE
-CREATE PROCEDURE userExists()
--- userExists PROCEDURE
+
+/*REASON : The query of the existence of a 
+  user will be constant in the use of the 
+  application.*/
+
+/*HOW IT WORKS : By using two parameters,
+  one for giving the userName to search
+  and other as the result of the procedure,
+  we find out if that user exists with a
+  simple query, if exists it will return
+  a 1 also known as true and if not 0 or false
+  **also if we need to see it we need to pass
+  a 1 or true to showR in order to see the
+  result or pass a 0 or false to avoid 
+  seeing the result** (check in depth the part
+  between symbols)*/
+
+DROP PROCEDURE userExists;
+
+CREATE PROCEDURE userExists(
+    IN userName VARCHAR(150),
+    IN showR BIT,
+    OUT isThere BIT
+)
+BEGIN
+    IF EXISTS (
+        SELECT user_name
+        FROM users
+        WHERE user_name = userName
+    )
+        THEN SET isThere = 1;
+    ELSE
+        SET isThere = 0;
+    END IF;
+
+    IF showR = 1 THEN
+        SELECT isThere;
+    END IF;
+END//
+
+DELIMITER ;
+
+CALL userExists('gerr', false, @isThere);
+
+
+DELIMITER //
 
 -- createUser PROCEDURE
 CREATE PROCEDURE createUser(
     IN userName VARCHAR(150),
     IN userType CHAR(1)
 )
-AS
 BEGIN
+    CALL userExists(LOWER(userName), false, @isThere);
+    IF @isThere = 1 THEN
+        INSERT INTO users(user_name, user_type) VALUES(userName, userType);              
+    END IF;
+    SELECT @isThere; 
+END //
 
-END//
+DELIMITER;
+
+CALL createUser('gerroar', 'b');
+
+DELETE FROM users WHERE id = 2;
+
+ALTER TABLE users AUTO_INCREMENT = 1;
 -- createUser PROCEDURE
 
 -- deleteUser PROCEDURE
@@ -77,4 +133,4 @@ END//
 -- getUsers PROCEDURE
 
 -- STORED PROCEDURES
-DELIMITER;
+
