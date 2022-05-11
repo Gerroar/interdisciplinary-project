716ecf06193eca7a1309eaed8b5f6998f47d4381
Germán <germanariasrodriguez@gmail.com>
2022-05-11 21:11:21 +0200

    Update database-structure.sql
    Updating createUser

--- Database/database-structure.sql
+++ Database/database-structure.sql
@@ -56,13 +56,10 @@ DELIMITER //
   we find out if that user exists with a
   simple query, if exists it will return
   a 1 also known as true and if not 0 or false
-  **also if we need to see it we need to pass
+  also if we need to see it we need to pass
   a 1 or true to showR in order to see the
   result or pass a 0 or false to avoid 
-  seeing the result** (check in depth the part
-  between symbols)*/
-
-DROP PROCEDURE userExists;
+  seeing the result*/
 
 CREATE PROCEDURE userExists(
     IN userName VARCHAR(150),
@@ -73,7 +70,7 @@ BEGIN
     IF EXISTS (
         SELECT user_name
         FROM users
-        WHERE user_name = userName
+        WHERE user_name = LOWER(userName)
     )
         THEN SET isThere = 1;
     ELSE
@@ -86,35 +83,58 @@ BEGIN
 END//
 
 DELIMITER ;
-
-CALL userExists('gerr', false, @isThere);
-
-
-DELIMITER //
+-- userExists PROCEDURE
 
 -- createUser PROCEDURE
+
+/*REASON : Automate the process of creating a new user
+  checking first if it exists or not*/
+
+/*HOW IT WORKS : Giving the userName(users), userType(users),
+ userImg(settings), userPhone(settings), userEmail(settings)
+ and userPass(settings). Will check if the user is already registered
+ if is not it will insert the values and give a 1 as a 
+ result, if it's registered it will give a 0 to indicate
+ that that user already exists */
+
+ DROP PROCEDURE createUser;
+DELIMITER //
+
 CREATE PROCEDURE createUser(
     IN userName VARCHAR(150),
-    IN userType CHAR(1)
+    IN userType CHAR(1),
+    IN userImg TEXT,
+    IN userPhone VARCHAR(31),
+    IN userEmail VARCHAR(320),
+    IN userPass VARCHAR(15),
+    OUT result BIT
 )
 BEGIN
-    CALL userExists(LOWER(userName), false, @isThere);
-    IF @isThere = 1 THEN
-        INSERT INTO users(user_name, user_type) VALUES(userName, userType);              
+    CALL userExists(userName, false, @isThere);
+    IF @isThere = 0 THEN
+
+        DECLARE userId INT;
+
+        SET 
+        SET result = 1;
+        INSERT INTO users(user_name, user_type) VALUES(LOWER(userName), LOWER(userType));
+        INSERT INTO settings()
+    ELSE
+        SET result = 0;
     END IF;
-    SELECT @isThere; 
+    SELECT result;
 END //
 
 DELIMITER;
 
-CALL createUser('gerroar', 'b');
-
-DELETE FROM users WHERE id = 2;
-
-ALTER TABLE users AUTO_INCREMENT = 1;
 -- createUser PROCEDURE
 
 -- deleteUser PROCEDURE
+
+DELIMITER //
+
+CREATE PROCEDURE deleteUser()
+DELIMITER ;
 -- deleteUser PROCEDURE
 
 -- createPost PROCEDURE
