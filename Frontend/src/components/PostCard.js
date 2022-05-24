c56889a73e76ba497cdf2ad172b8427ac14fb971
Germán <germanariasrodriguez@gmail.com>
2022-05-24 19:40:30 +0200

    changes

--- Frontend/src/components/PostCard.js
+++ Frontend/src/components/PostCard.js
@@ -4,11 +4,13 @@ export default function PostCard({ post }) {
 
     return (
         <article className="post-article">
-            <img src={post.img} alt={post.title} />
-            <p>{post.user_n}</p>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-            <p>{post.post_d}</p>
+                <div id="pa-div-1">
+                    <img src={post.img} alt={post.title} />
+                    <p>{post.user_n}</p>
+                </div>
+                <h2>{post.title}</h2>
+                <div id="pa-div-2">{post.content}</div>
+                <p>{post.post_d}</p>
         </article>
     );
 }
