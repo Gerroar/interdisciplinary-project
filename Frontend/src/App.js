b304da17b704bfde1e920277fb3ae20aa7e716b0
MarioCDS <101996644+MarioCDS@users.noreply.github.com>
2022-05-24 13:02:57 +0200

    Profile changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HandleInfoPage from "./pages/HandleInfoPage";
 import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HandleInfoPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/home/profile" element={<ProfilePage />} />
       </Routes>
     </main>
   );
